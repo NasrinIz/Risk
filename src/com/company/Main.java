ab620eeeee3c3cc08a0f3ee6f7b5482a2f42e340
Banafshe <Banafshe@DESKTOP-44UR5QV.lan>
2017-09-13 14:15:29 -0400

    Test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Testing in both IDE s......");
+        System.out.println("Testing in both IDE s Test......");
 	// write your code here
     }
 }
