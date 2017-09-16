dcba5459a4171c2fe6ea341e4db7006989904bce
Unknown <vaibhavjain01@hotmail.com>
2017-09-16 13:46:49 -0400

    Map loaded in memory, but validation still remains.

--- src/com/company/Main.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Testing in both IDE s Test......");
-	// write your code here
-    }
-}
