df998694c51d198fcaeece554f0505a97a64d143
nasrinizadivahedi <izadivahedi.nasrin@gmail.com>
2017-10-07 17:25:55 -0400

    Changed to MVC

---
+++ App/src/controller/MainController.java
@@ -0,0 +1,79 @@
+package controller;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import model.GameConfig;
+import model.Maps;
+import view.MainWindow;
+import view.StarterWindow;
+
+public class MainController {
+	private StarterWindow starterView;
+	private MainWindow mainWindow;
+	private GameConfig gameConfig;
+	private Maps objMap = null;
+	
+	public MainController(StarterWindow starterView){
+		this.starterView = starterView;
+		this.starterView.addMenuItemNewGameActionListener(new NewGameListener());
+	}
+	
+	private class NewGameListener implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			starterView.showStarterForm();
+			starterView.addRadioLoadMapActionListener(new loadMapListener());
+			starterView.addRadioSelectMapActionListener(new selectMapListener());
+			starterView.addRadioCreateMapActionListener(new CreateMapListener());
+			starterView.addSubmitButtontActionListener(new SubmitButtonListener());
+		}
+	}
+	
+	private class loadMapListener implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+		
+		}
+	}
+	
+	private class selectMapListener implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			starterView.showSelectMapForm();
+		}
+	}
+	
+	private class CreateMapListener implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+		
+		}
+	}
+	
+	private class SubmitButtonListener implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+
+			Integer playerNum = starterView.getPlayerNumbers();
+			String selectedMap = starterView.getSelectedMap();
+			
+			String mapTxtLoc = String.format("Resources//Maps//%s.map", selectedMap);
+			String mapImgLoc = String.format("Resources//Maps//%s.bmp", selectedMap);
+			
+			objMap = new Maps(mapTxtLoc);
+			objMap.setMapName(selectedMap);
+			
+			mainWindow = new MainWindow();
+			mainWindow.addCountryButtons(objMap);
+			mainWindow.setVisible(true);
+			starterView.setVisible(false);
+		}
+	}
+	
+	
+}
---
+++ App/src/view/StarterWindow.java
@@ -0,0 +1,145 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JRadioButton;
+
+public class StarterWindow extends JFrame {
+	private JMenuBar menuBar;
+	private JMenu menu;
+	private JMenuItem menuItemNewGame;
+	private JLabel lblNumPlayers;
+	private JLabel lblMapSelect;
+	private JComboBox<Integer> playerNum;
+	private JRadioButton radioLoadMap;
+	private JRadioButton radioSelectMap;
+	private JRadioButton radioCreateMap;
+	private JComboBox<String> mapList;
+	private JButton submitButton;
+
+	ButtonGroup group = new ButtonGroup();
+
+	private BorderLayout borderLayout;
+
+	public StarterWindow() {
+		menuBar = new JMenuBar();
+		menu = new JMenu("File");
+		menuItemNewGame = new JMenuItem("New Game");
+		setSize(getMaximumSize());
+		this.initWindow();
+		this.initMenuBar();
+		this.initContentPane();
+	}
+
+	private void initWindow() {
+		setTitle("Risk");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 567, 489);
+		setExtendedState(java.awt.Frame.MAXIMIZED_BOTH);
+	}
+
+	private void initContentPane() {
+		borderLayout = new BorderLayout();
+		getContentPane().add(menuBar);
+		this.setLayout(borderLayout);
+	}
+
+	private void initMenuBar() {
+		setJMenuBar(menuBar);
+		menuBar.add(menu);
+		menu.add(menuItemNewGame);
+	}
+
+	public void addMenuItemNewGameActionListener(ActionListener listenForMenuItemNewGame) {
+		menuItemNewGame.addActionListener(listenForMenuItemNewGame);
+	}
+
+	public void addRadioLoadMapActionListener(ActionListener listenForLoadMap) {
+		radioLoadMap.addActionListener(listenForLoadMap);
+	}
+
+	public void addRadioSelectMapActionListener(ActionListener listenForSelectMap) {
+		radioSelectMap.addActionListener(listenForSelectMap);
+	}
+
+	public void addRadioCreateMapActionListener(ActionListener listenForCreateMap) {
+		radioCreateMap.addActionListener(listenForCreateMap);
+	}
+
+	public void addMapListActionListener(ActionListener listenForMapList) {
+		mapList.addActionListener(listenForMapList);
+	}
+
+	public void addSubmitButtontActionListener(ActionListener listenForSubmitButton) {
+		submitButton.addActionListener(listenForSubmitButton);
+	}
+
+	public void showStarterForm() {
+
+		radioLoadMap = new JRadioButton("Load map");
+		radioLoadMap.setBounds(10, 80, 200, 30);
+		radioSelectMap = new JRadioButton("Select map");
+		radioSelectMap.setBounds(220, 80, 200, 30);
+		radioCreateMap = new JRadioButton("Create map");
+		radioCreateMap.setBounds(430, 80, 200, 30);
+
+		lblNumPlayers = new JLabel("Number of Human Players: ");
+		lblNumPlayers.setBounds(10, 40, 200, 20);
+
+		Integer[] playerNums = new Integer[] { 2, 3, 4, 5, 6 };
+
+		playerNum = new JComboBox<>(playerNums);
+		playerNum.setBounds(200, 40, 200, 20);
+
+		submitButton = new JButton("Submit");
+		submitButton.setBounds(10, 500, 100, 30);
+		//submitButton.setEnabled(false);
+		group = new ButtonGroup();
+		group.add(radioLoadMap);
+		group.add(radioSelectMap);
+		group.add(radioCreateMap);
+
+		this.getContentPane().add(lblNumPlayers);
+		this.getContentPane().add(playerNum);
+		this.getContentPane().add(radioLoadMap);
+		this.getContentPane().add(radioSelectMap);
+		this.getContentPane().add(radioCreateMap);
+		this.getContentPane().add(submitButton);
+		this.getContentPane().repaint();
+	}
+
+	public void showSelectMapForm() {
+		lblMapSelect = new JLabel("Select a map: ");
+		lblMapSelect.setBounds(10, 120, 200, 20);
+
+		String[] mapTitles = new String[] { "Atlantis", "DiMul", "Europe", "Old Yorkshire", "Polygons", "Twin Volcano",
+				"USA", "World" };
+
+		mapList = new JComboBox<>(mapTitles);
+		mapList.setBounds(120, 120, 200, 20);
+
+		this.getContentPane().add(lblMapSelect);
+		this.getContentPane().add(mapList);
+		this.getContentPane().repaint();
+	}
+
+	public Integer getPlayerNumbers() {
+		Integer numPlayers = (Integer)playerNum.getSelectedItem();
+		return numPlayers;
+	}
+
+	public String getSelectedMap() {
+		String selectedMap = String.valueOf(mapList.getSelectedItem());
+		return selectedMap;
+	}
+
+}
