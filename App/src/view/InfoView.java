862477ff652245f505938268819e4e3fd0f3b727
nasrinizadivahedi <izadivahedi.nasrin@gmail.com>
2017-10-17 13:34:06 -0400

    Information view changes

--- App/src/view/InfoView.java
+++ App/src/view/InfoView.java
@@ -12,9 +12,6 @@ import javax.swing.border.TitledBorder;
  * @author Team20
  */
 public class InfoView extends JPanel {
-	private JLabel terrNameLbl;
-	private JLabel terrNumArmiesLbl;
-	private JLabel terrAdjacentCountriesLbl;
 	private JTextArea InfoTextArea;
 	public InfoView() {
 		this.showTerritoryPanel();
@@ -26,16 +23,10 @@ public class InfoView extends JPanel {
 	public void showTerritoryPanel() {
 		this.setBackground(Color.GRAY);
 		this.setLayout(new FlowLayout());
-		terrNameLbl = new JLabel("Territory Name: ");
-		terrNumArmiesLbl = new JLabel("Number of Armies: ");
-		terrAdjacentCountriesLbl = new JLabel("Adjacent countries: ");
 		InfoTextArea = new JTextArea();
 		InfoTextArea.setRows(10);
 		
 		this.setBounds(1024, 0, 350, 768);
-//		this.add(terrNameLbl);
-//		this.add(terrNumArmiesLbl);
-//		this.add(terrAdjacentCountriesLbl);
 		this.add(InfoTextArea);
 		TitledBorder border = new TitledBorder("Information Panel");
 	    border.setTitleJustification(TitledBorder.CENTER);
