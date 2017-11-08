d548323ad9d553e9f5bb67c3487fd8c678ad9f94
NasrinI <izadivahedi.nasrin@gmail.com>
2017-11-07 19:56:31 -0500

    Removing redundant logs

--- src/view/PlayerInformationView.java
+++ src/view/PlayerInformationView.java
@@ -12,25 +12,23 @@ import java.util.Observer;
 /**
  * This view shows the information of players
  */
-public class PlayerInformationView extends JPanel implements Observer{
+public class PlayerInformationView extends JPanel implements Observer {
     private JTextArea InfoTextArea;
 
-    public PlayerInformationView()
-    {
+    public PlayerInformationView() {
         this.showInfoPanel();
     }
 
     /**
      * Show error information
      */
-    public void showInfoPanel()
-    {
+    public void showInfoPanel() {
         this.setBackground(Color.BLUE);
         this.setLayout(new FlowLayout());
         this.setBounds(1024, 268, 255, 100);
         InfoTextArea = new JTextArea();
-        InfoTextArea.setRows(2);
-        InfoTextArea.setBounds(1024, 268, 200, 50);
+        InfoTextArea.setRows(3);
+        InfoTextArea.setBounds(1024, 268, 200, 100);
         this.add(InfoTextArea);
 
         TitledBorder border = new TitledBorder("Phase View");
@@ -43,33 +41,12 @@ public class PlayerInformationView extends JPanel implements Observer{
     /**
      * @param info error to show in panel
      */
-    public void showPlayerInformationView(String info)
-    {
+    private void showPlayerInformationView(String info) {
         InfoTextArea.setText(info);
     }
+
     @Override
     public void update(Observable o, Object arg) {
-        String playerName = ((GameConfig) o).getCurrentPlayer().getName();
-        Integer gamePhase = ((GameConfig) o).getGamePhase();
-        String gamePhaseName = " ";
-
-        switch (gamePhase) {
-            case 1:
-                gamePhaseName = "STARTUP";
-                break;
-            case 2:
-                gamePhaseName = "REINFORCEMENT";
-                break;
-            case 3:
-                gamePhaseName = "FORTIFICATION";
-                break;
-            case 4:
-                gamePhaseName = "ATTACK";
-                break;
-        }
-
-        //this.showPlayerInformationView(playerName + gamePhaseName);
         this.showPlayerInformationView(((GameConfig) o).gamePhaseStr);
-
     }
 }
