cba137027b2e454818e0e5ad737a8a23f5ae38fd
NasrinI <izadivahedi.nasrin@gmail.com>
2017-11-29 16:04:43 -0500

    Removing test map

--- src/view/StarterWindow.java
+++ src/view/StarterWindow.java
@@ -317,7 +317,7 @@ public class StarterWindow extends JFrame {
         lblMapSelect.setBounds(10, 120, 200, 20);
 
         String[] mapTitles = new String[]{"Atlantis", "DiMul", "Europe", "Old Yorkshire", "Polygons", "Twin Volcano",
-                "USA", "World", "valid_1", "vj_test", "3D Cliff", "UnconnectedContinent"};
+                "USA", "World", "valid_1", "3D Cliff", "UnconnectedContinent"};
 
         mapList = new JComboBox<>(mapTitles);
         mapList.setBounds(220, 120, 200, 20);
